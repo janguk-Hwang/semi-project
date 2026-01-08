365915a79ea7209aaa43482b19aa36254e27c5d0
dbsekdgh <dbsekdgh09@gmail.com>
2026-01-08 10:08:22 +0900

    admindto

--- src/main/java/com/example/demo/mapper/AdminMapper.java
+++ src/main/java/com/example/demo/mapper/AdminMapper.java
@@ -1,7 +1,17 @@
 package com.example.demo.mapper;
 
+import com.example.demo.dto.User_rolesDto;
+import com.example.demo.dto.UsersDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface AdminMapper {
+    int count(Map<String,Object> map);
+    List<UsersDto>userlist(Map<String,Object> map);
+    int admindelete(int member_id);
+    int roleupdate(UsersDto dto);
+    User_rolesDto isadmin(int member_id);
 }
